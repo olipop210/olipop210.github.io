2f025b03645715806dbcf5538904331db200cefe
Oliwier Popielarczyk <71080644+olipop210@users.noreply.github.com>
2025-10-16 10:23:12 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
+import Learning from './Learning';
+
 
 function App() {
+
+  const [panelData, setPanelData] = useState<string | null>(null);
+
+  const [icon1Gradient, setIcon1Gradient] = useState('radial-gradient(circle at 100%, rgb(189, 83, 255) 0%, rgb(33, 4, 255) 100%)');
+  const [icon2Gradient, setIcon2Gradient] = useState('radial-gradient(135deg, rgb(189, 83, 255) 0%, rgb(33, 4, 255) 100%)');
+  const [icon3Gradient, setIcon3Gradient] = useState('radial-gradient(135deg, rgb(189, 83, 255) 0%, rgb(33, 4, 255) 100%)');
+
+  const gradientUpdate = (e: React.MouseEvent, cardClass: string) => {
+    try {
+      const card = document.querySelector(`.${cardClass}`) as HTMLElement;
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      console.log(x, y);
+    }
+    catch (err) {
+      console.log(err);
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header>
+        <h1>Lorem Ipsum</h1>
       </header>
-    </div>
+      <article>
+        <section onClick={() => setPanelData('about-me')} className='about-me-card'>
+          <Icon className='acc-icon' />
+          <p className='hiddenText'>O mnie</p>
+        </section>
+        <section onClick={() => setPanelData('learning-materials')} className='learning-materials-card'>
+          <Icon className='learn-icon' />
+          <p className='hiddenText'>Materiały do nauki</p>
+        </section>
+        <section onClick={() => setPanelData('projects')} className='projects-card'>
+          <Icon className='proj-icon' />
+          <p className='hiddenText'>Projekty</p>
+        </section>
+      </article>
+      <section style={{ opacity: panelData ? 1 : 0 }} className='panel-title'>
+        {panelData === 'about-me' && <h2>O mnie</h2>}
+        {panelData === 'learning-materials' && <h2>Materiały do nauki</h2>}
+        {panelData === 'projects' && <h2>Projekty</h2>}
+        <div onClick={() => setPanelData(null)} className='close-icon'></div>
+      </section>
+      <section style={{ transform: panelData ? 'translateY(0dvh)' : 'translateY(75dvh)' }} className='information-panel'>
+        {panelData === 'about-me' && <p>Informacje o mnie</p>}
+        {panelData === 'learning-materials' && <Learning />}
+        {panelData === 'projects' && <p>Projekty</p>}
+      </section>
+    </main>
   );
+}
+
+const Icon = ({ className }: { className: string }) => {
+
+  const gradientUpdate = (e: React.MouseEvent) => {
+    setIsHovered(true);
+    try {
+      const icon = document.querySelector(`.${className}`) as HTMLElement;
+      const rect = icon.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      setGradient(`radial-gradient(circle at ${x}px ${y}px, rgb(189, 83, 255) 0%, rgb(33, 4, 255) 100%)`);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [gradient, setGradient] = useState('radial-gradient(circle at 100%, rgb(189, 83, 255) 0%, rgb(33, 4, 255) 100%)');
+
+  return (
+    <div onMouseMove={gradientUpdate} onMouseLeave={() => setIsHovered(false)} style={{ background: isHovered ? gradient : '' }} className={className}></div>
+  )
 }
 
 export default App;
---
+++ src/Learning.tsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import './Learning.scss';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
+
+
+const Learning = () => {
+
+    const learningResources = [
+        {
+            title: 'Java notatki v1.0',
+            desc: 'Notatki z Javy, obejmuje zakres od kompletnych podstaw do bardziej zaawansowanych tematów jak klasy wyjątki i binarki',
+            link: 'java.pdf'
+        },
+        {
+            title: 'Zbrodnie Ha*dukiewicza',
+            desc: 'Notatki z C++ obejmujące podstawy języka, wskaźniki, klasy i dziedziczenie',
+            link: 'hajdukiewicz.pdf'
+        },
+        {
+            title: 'F*stowicz ty k*rwo',
+            desc: 'Notatki z lekcji urządzeń techniki komputerowej, obejmujące elementy organizacji systemów komputerowych, architekturę komputerów, logiki dla informatyków, układów cyfrowych i budowy komputerów',
+            link: 'fastowicz.pdf'
+        }
+    ]
+
+    return (
+        <>
+            <ul className='learning-list'>
+                {learningResources.map((resource, index) => <LearningResource key={index} {...resource} />)}
+            </ul>
+        </>
+    );
+};
+
+type LearningResourceProps = {
+    title: string;
+    desc: string;
+    link: string;
+};
+
+const LearningResource = ({ title, desc, link }: LearningResourceProps) => {
+
+    const [showingDesc, setShowingDesc] = useState(false);
+
+    const [showingPDF, setShowingPDF] = useState(false);
+
+    const [pagesArr, setPagesArr] = useState<number[]>([]);
+
+    const openPDF = () => {
+        setShowingPDF(true);
+        setShowingDesc(false);
+    }
+
+    const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+        const pages = Array.from({ length: numPages }, (_, i) => i + 1);
+        setPagesArr(pages);
+        console.log('Pages array: ', pages);
+    }
+
+    return (
+        <>
+            <li>
+                <h3>{title}</h3>
+                <section onClick={() => setShowingDesc(!showingDesc)}>
+                    <Document className={`${showingDesc ? 'blurred' : ''} unblurred`} renderMode='canvas' file={link} >
+                        <Page renderAnnotationLayer={false} canvasBackground='transparent' renderTextLayer={false} pageNumber={1} width={300} />
+                    </Document>
+                    {
+                        showingDesc ? <div className='desc'>
+                            <p>{desc}</p>
+                            <b onClick={openPDF}>Otwórz</b>
+                            <a href={link} download>Pobierz</a>
+                        </div> : null
+                    }
+                </section>
+            </li>
+            {
+                showingPDF ? <section className='pdf-viewer-container'> <div className='pdf-viewer'>
+
+                    <Document renderMode='canvas' onLoadSuccess={onDocumentLoadSuccess} file={link} >
+                        {
+                            pagesArr.map((pageNum) => (
+                                <Page key={pageNum} renderAnnotationLayer={false} canvasBackground='transparent' renderTextLayer={false} pageNumber={pageNum} width={800} />
+                            ))
+                        }
+                    </Document>
+                </div> <div className='close-btn' onClick={() => setShowingPDF(false)}></div> </section> : null
+            }
+        </>
+    );
+};
+
+export default Learning;
