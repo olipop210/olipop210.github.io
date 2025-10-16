d0ffa5a30f3a5b40421c3ee9d7029ad914d68008
Oliwier Popielarczyk <71080644+olipop210@users.noreply.github.com>
2025-10-16 12:11:01 +0200

    Update Learning.tsx

--- src/Learning.tsx
+++ src/Learning.tsx
@@ -3,10 +3,7 @@ import './Learning.scss';
 
 import { Document, Page, pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 
 const Learning = () => {
